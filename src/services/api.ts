41832de27c70a1913af6f57fd2b260dc51549b73
MilkWind <2250957906@qq.com>
2025-06-01 16:22:05 +0800

    add environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useI18n } from 'vue-i18n'
 
-const API_BASE_URL = 'http://localhost:5000/api'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 // Create axios instance
 const api = axios.create({
--- src/stores/aiConfig.ts
+++ src/stores/aiConfig.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 import { useToast } from 'vue-toastification'
 import { useI18n } from 'vue-i18n'
 
-const API_BASE_URL = 'http://localhost:5000/api'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 interface AIConfig {
   _id: string
